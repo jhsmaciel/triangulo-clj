31448f664d52697c2338886afe8c438b8e05db19
Joao Henrique <joao.henrique@joao.henrique>
2022-09-15 10:49:09 -0300

    Feat(): Initial release triangle calcs

--- src/triangulo/core.clj
+++ src/triangulo/core.clj
@@ -1,5 +1,5 @@
 (ns triangulo.core
-  (:require [clojure.math :as math]))
+  (:require [clojure.math :refer [sqrt pow acos to-degrees round]]))
 
 (defn calc-perimetro
   "Calcula o perimetro do triangulo, dado A B e C"
@@ -7,71 +7,115 @@
   (+ a b c))
 
 (defn calc-radianos
-  "TODO: Calcular radianos dado lados a b e c de um triangulo"
+  "Calcula radiano ∠A, dado A B C."
   [a b c]
-  )
+  (-> (pow b 2)
+      (+ (pow c 2))
+      (- (pow a 2))
+      (/ (* b c 2))
+      acos))
 
 (defn calc-angulo
-  "TODO: Calcula o ângulo ∠A, dado A B C."
+  "Calcula o ângulo ∠A, dado A B C."
   [a b c]
-  )
+  (-> (pow b 2)
+      (+ (pow c 2))
+      (- (pow a 2))
+      (/ (* 2 b c))
+      acos
+      to-degrees))
 
 (defn calc-area
-  "TODO: Calcula a área de um triângulo usando a formula de Heron."
+  "Calcula a área de um triângulo usando a formula de Heron."
   [a b c]
-  )
+  (let [s (/ (calc-perimetro a b c) 2)
+        s-a (- s a)
+        s-b (- s b)
+        s-c (- s c)]
+    (-> s
+        (* s-a)
+        (* s-b)
+        (* s-c)
+        sqrt)))
 
 (defn calc-altura
-  "TODO: Calcula altura de A, dado a AREA."
+  "Calcula altura de A, dado a AREA."
   [a area]
-  )
+  (-> 2
+      (* area)
+      (/ a)))
 
 (defn equilateral?
   "TODO: Verifica se o triangulo é equilateral"
   [a b c]
-  )
+  (== a b c))
 
 (defn isosceles?
-  "TODO: Verifica se pelo menos dois lados sao iguais."
+  "Verifica se pelo menos dois lados sao iguais."
   [a b c]
-  )
+  (->> [(== a b) (== b c) (== a c)]
+       (some true?)
+       boolean))
 
 (defn escaleno?
-  "TODO: Verifica se os lados dos triangulos sao diferentes entre si."
+  "Verifica se os lados dos triangulos sao diferentes entre si."
   [a b c]
-  )
+  (not (isosceles? a b c)))
+
+(defn- calcula-angulos-e-compara
+  "Função interna que calcula os três ângulos do triângulo faz uma comparação desejada com o ângulos e retorna true se alguma daquelas condições satisfazer"
+  [a b c funcao-comparacao funcao-que-decide]
+  (->> [(calc-angulo a b c) (calc-angulo b c a) (calc-angulo c a b)]
+       (map funcao-comparacao)
+       (funcao-que-decide true?)
+       boolean))
+
+(defn- angulo-igual-que-90?
+  "Verifica se o ângulo indicado é igual a 90 (arredondando)"
+  [angulo]
+  (= (round angulo) 90))
+
+(defn- angulo-maior-que-90?
+  "Verifica se o ângulo indicado é maior que 90"
+  [angulo]
+  (> (round angulo) 90))
+
+(defn- angulo-menor-que-90?
+  "Verifica se o ângulo indicado é menor que 90"
+  [angulo]
+  (< (round angulo) 90))
 
 (defn retangulo?
-  "TODO: Verifica se é um triangulo retangulo, cujos angulos são iguais a 90o.
+  "Verifica se é um triangulo retangulo, cujos angulos são iguais a 90o.
   O resultado não é exato, dado que cada angulo é arredondado utilizando clojure.math/round."
   [a b c]
-  )
+  (calcula-angulos-e-compara a b c angulo-igual-que-90? some))
 
 (defn obtuso?
-  "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
+  "Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (calcula-angulos-e-compara a b c angulo-maior-que-90? some))
 
 (defn agudo?
-  "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
+  "Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (calcula-angulos-e-compara a b c angulo-menor-que-90? every?))
 
 (defn gerar-dados-completos
   [a b c]
   (let [area (calc-area a b c)]
-        {:lados [a b c]
-         :retagulo (retangulo? a b c)
-         :obtuso (obtuso? a b c)
-         :agudo (agudo? a b c)
-         :escaleno (escaleno? a b c)
-         :isosceles (isosceles? a b c)
-         :equilateral (equilateral? a b c)
-         :area area
-         :altura [(calc-altura a area)
-                  (calc-altura b area)
-                  (calc-altura c area)]
-         :angulos [(calc-angulo a b c)
+    {:lados       [a b c]
+     :retagulo    (retangulo? a b c)
+     :obtuso      (obtuso? a b c)
+     :agudo       (agudo? a b c)
+     :escaleno    (escaleno? a b c)
+     :isosceles   (isosceles? a b c)
+     :equilateral (equilateral? a b c)
+     :area        area
+     :altura      [(calc-altura a area)
+                   (calc-altura b area)
+                   (calc-altura c area)]
+     :angulos     [(calc-angulo a b c)
                    (calc-angulo b c a)
                    (calc-angulo c a b)]}))
 
